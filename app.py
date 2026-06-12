ddd607c1d2cef06d232b7d7a11fd385490624a26
P Mahir Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-06-12 12:33:49 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # your code here
 import os
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
